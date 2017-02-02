c5b7e5b7fac25a8d06ab310f2484ab4baa3ed58e
Mike Doudkin <mdoudkin@gmail.com>
2017-02-03 00:19:18 +0300

    IMplement search algorithm, abandon cut size subtask.

--- distance.py
+++ distance.py
@@ -1,8 +1,7 @@
 import sys
-import networkx as nx
 
 
-def _parse_file(filename):
+def parse_file(filename):
     with open(filename) as f:
         lines = f.readlines()
     # Assumption 1: lines are not surrounded with quotes, no empty lines in file
@@ -10,40 +9,59 @@ def _parse_file(filename):
     return lines
 
 
-def distance(initial_data, city_a, city_b):
-    graph = nx.Graph()
+def create_graph(initial_data):
+    graph = dict()
+    for node_a, node_b, distance in initial_data:
+        # Assumption 2: distances must be greater than or equal to 0
+        distance = int(distance)
+        if distance < 0:
+            raise ValueError('Negative distances are not allowed.')
+        graph.setdefault(node_a, dict()).setdefault(node_b, []).append(distance)
+        graph.setdefault(node_b, dict()).setdefault(node_a, []).append(distance)
+    return graph
 
-    for line in initial_data:
-        graph.add_edge(line[0], line[1], weight=int(line[2]))
 
-    # Assumption 2: graph is connected (there is a path from any node to any other)
-    if not nx.is_connected(graph):
-        raise Exception('The graph must be connected')
+def shortest_path(graph, start, end):
+    if graph.get(start, None) is None:
+        raise KeyError(start)
+    if graph.get(end, None) is None:
+        raise KeyError(end)
+
+    queue = [{'path': [start], 'length': 0}]
+    extended_nodes = []
+
+    while len(queue):
+        path_to_extend = queue.pop(0)
+
+        node = path_to_extend['path'][-1]
+        if node == end:
+            return path_to_extend
+
+        extended_nodes.append(node)
+        for next_node, distances in graph[node].items():
+            if next_node not in extended_nodes:
+                queue.append({
+                    'path': path_to_extend['path'] + [next_node],
+                    'length': path_to_extend['length'] + min(distances)
+                })
+        queue = sorted(queue, key=lambda path: path['length'])
+
+    return None
+
+
+def distance(initial_data, node_a, node_b):
+    graph = create_graph(initial_data)
+
     try:
-        shortest_path = nx.shortest_path(graph, city_a, city_b, 'weight')
+        return shortest_path(graph, node_a, node_b)
     except KeyError as e:
         raise Exception('There is no node named %s' % e)
 
-    # Could use a list comprehension here, but it lacks readability
-    lengths = []
-    for i in range(len(shortest_path) - 1):
-        lengths.append(graph.get_edge_data(shortest_path[i], shortest_path[i+1])['weight'])
-    shortest_path_length = sum(lengths)
-
-    equal_parts = nx.kernighan_lin_bisection(graph)
-    cut_size = nx.cut_size(graph, *equal_parts)
-
-    return {
-        'shortest path': shortest_path,
-        'shortest path length': shortest_path_length,
-        'cut size': cut_size
-    }
-
 
 if __name__ == "__main__":
-    filename, city_a, city_b = sys.argv[1:]
-    initial_data = _parse_file(filename)
+    filename, node_a, node_b = sys.argv[1:]
+    initial_data = parse_file(filename)
     try:
-        print(distance(initial_data, city_a, city_b))
+        print('The shortest path from {0} to {1} is {2}'.format(node_a, node_b, distance(initial_data, node_a, node_b)))
     except Exception as e:
         print(e)
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 #  ReadMe
-This project is a test problem which involves graphs.
+This project is a shortest path finding algorithm implementation
 
 ## Requirements
 This program was tested on python 3.6, so execution is guaranteed on this version
@@ -8,31 +8,24 @@ This program was tested on python 3.6, so execution is guaranteed on this versio
 
 ```
 git clone {this repo name}
-pip install -r requirements.txt
 ```
 
 ## Usage
 in terminal, type
 ```
-python distance.py ${path/to/file} ${city_a} ${city_b}
+python distance.py ${path/to/file} ${node_a} ${node_b}
 ```
 for example,
 ```
-python distance.py ~/my_file.txt Washington "Los Angeles"
+python distance.py ~/my_file.txt Moscow "St. Petersburg"
 ```
-The output will be a dictionary of 3 keys:
-
-* shortest path: The path from city_a to city_b
-* shortest path length: length of the path
-* cut size: number of edges to be removed from graph to separate it in 2 (almost) equal parts
 
 ## Assumptions
 * no empty lines inbetween and no quotes surrounding lines are allowed, and each line of the input file has the following format:
 ```
 city_a, city_b, distance
 ```
-third value in line must be an integer, for example
+* distance must be an non-negative integer, for example
 ```
 New York, Washington, 1
 ```
-* the graph is connected, i.e. any node is reachable from any other node
